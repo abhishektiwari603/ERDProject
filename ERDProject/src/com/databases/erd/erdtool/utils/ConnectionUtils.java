c211f5c85824728deca3ac6da569f506acad37d4
abhishek <abhishektiwari603@gmail.com>
2016-07-17 13:52:39 +0530

    Added connection utility method for PostgreSQL.

--- ERDProject/src/com/databases/erd/erdtool/utils/ConnectionUtils.java
+++ ERDProject/src/com/databases/erd/erdtool/utils/ConnectionUtils.java
@@ -9,15 +9,46 @@ import java.sql.SQLException;
  * @author ABHISHEK
  * @Since Jul 17, 2016
  */
-public class ConnectionUtils {
-	
-	public static Connection getMySQLConnection(String host, String port, String userName, String password)
-	          throws SQLException, ClassNotFoundException
-	{					
-		    String URL = "jdbc:mysql://" + host + "/" + port +"/";
-			Class.forName("com.mysql.jdbc.Driver");	
-			Connection conn = DriverManager.getConnection(URL, userName, password);
-			return conn;			
-	}
+public class ConnectionUtils
+{
+
+    /**
+     * 
+     * @param host
+     * @param port
+     * @param userName
+     * @param password
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public static Connection getMySQLConnection(String host, String port, String userName, String password)
+            throws SQLException, ClassNotFoundException
+    {
+        String URL = "jdbc:mysql://" + host + "/" + port + "/";
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(URL, userName, password);
+        return conn;
+    }
+
+    /**
+     * 
+     * @param host
+     * @param port
+     * @param userName
+     * @param password
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public static Connection getPostgreConnection(String host, String port, String userName, String password)
+            throws SQLException, ClassNotFoundException
+    {
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://" + host + ":" + port + "/", userName, password);
+        return con;
+
+    }
 
 }
