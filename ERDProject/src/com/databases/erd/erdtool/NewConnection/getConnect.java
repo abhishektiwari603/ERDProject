f91ae0efa7a85140dd4c92c14047926d1aefc549
abhishek <abhishektiwari603@gmail.com>
2016-07-17 13:31:27 +0530

    Adding ignore file.

--- ERDProject/src/com/databases/erd/erdtool/NewConnection/getConnect.java
+++ ERDProject/src/com/databases/erd/erdtool/NewConnection/getConnect.java
@@ -2,7 +2,6 @@ package com.databases.erd.erdtool.NewConnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 import com.databases.erd.erdtool.connectionModel.ConnDialogModel;
 
